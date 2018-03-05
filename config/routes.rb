be8b8301c7f85662ea01020509af81807796ab62
daniel yanez <danielyanezavila@gmail.com>
2018-03-05 10:15:28 -0300

    carrito compras

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  get 'products/index'
+
+  devise_for :users
+
+  resources :products, only: [:index] do
+    resources :orders, only: [:create]
+  end
+  resources :orders, only: [:index, :destroy]do
+    collection do
+      delete 'empty_cart'
+    end
+  end
+
+  root 'products#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+end
